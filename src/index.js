f11ae023c127d137d42cb0b340c001a7bf753764
Anurag <89177393+xyrocodes@users.noreply.github.com>
2023-05-27 00:18:39 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,11 +6,9 @@ import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-<React.StrictMode>
-  <BrowserRouter>
+<BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
-</React.StrictMode>
-
+  </React.StrictMode>
+</BrowserRouter>
 );
